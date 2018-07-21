242581dbc388527ca62b55552812b6170e0b2e44
shalandy <shalandy@berkeley.edu>
2018-07-21 11:53:37 -0700

    onboard msg

--- components/onboarding.js
+++ components/onboarding.js
@@ -10,7 +10,17 @@ module.exports = function (controller) {
             if (err) {
                 console.log(err);
             } else {
-                convo.say('I am a bot that has just joined your workspace!');
+              bot.reply(convo, {
+                attachments:[
+                    {
+                      fallback: "Hello, I’m your SlackPal! :wave: \n\n\n :money_with_wings: Message me your receipts and I will handling all the bill splitting and payments.",
+                      pretext: ":wave: Hello, I’m your SlackPal! :wave: \n\n\n :money_with_wings: Message me your receipts and I will handling all the bill splitting and payments. \n🤝Introduce me to your team:",
+                      title: "Feel free to paste this message in #general",
+                      text: "Hi! I’ve just added SlackPal to this workspace—it’s a slack bot that will handle payments whenever you need to split the bill. Next time you pick up the tab, just direct message @slackpal your receipt to save you the hassle.",
+                      color: "#7CD197"
+                    }
+                ]
+              });
             }
         });
     });
