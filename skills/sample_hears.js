864ba25dd4126d9894b16d085be5b5c091cce898
Kyle Lim <kylelim8998@gmail.com>
2018-07-21 12:33:12 -0700

    user group message init

--- skills/sample_hears.js
+++ skills/sample_hears.js
@@ -54,9 +54,22 @@ module.exports = function(controller) {
 
     controller.hears(['^make chat$'], 'direct_message,direct_mention', function(bot, message) {
 
-        require('../server.js').web.mpim.open({'users':'UBGETPENP,UBGLM9Z26,UBGMVP22H'}).then((res) => {
+        creator_id = message.user
+
+        bot.api.mpim.open({'users':'UBGETPENP,UBGLM9Z26,UBGMVP22H'}, function(err, res) {
             console.log(res)
+            console.log(res.group.id)
+            group_id = res.group.id
+
+            bot.api.chat.postMessage({'channel':group_id, 'text': ':wave: Hello, it looks like <@'+creator_id+'> picked up the tab for a recent purchase. \n\n\n When prompted, please select the item that belongs to you so I can help you arrange the payment.'}, function(err, res) {
+                console.log(res)
+            })
         })
+        //
+        // require('../server.js').web.mpim.open({'users':'UBGETPENP,UBGLM9Z26,UBGMVP22H'}).then((res) => {
+        //     console.log(res)
+        //     res.group.id
+        // })
         bot.reply(message, "ok opening chat")
     });
 
